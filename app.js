3f42c42891b1abfd6dc10cf25711995fc518daec
jutivia <rukkylaw.tl@gmail.com>
2021-07-25 18:03:39 +0100

    addeed front-end

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express= require('express');
 const app= express();
+const tasks= require('./routes/tasks')
 
-app.use(express.static('/public'));
+
+
+app.use(express.static('./public'));
+
 
 app.get('/hello', (req, res)=>{
     res.send('task manager app')
 })
+
+app.use('/api/v1/tasks', tasks)
 
 const port= 3000;
 app.listen(port, ()=>{console.log(`server is listening on ${port}`)})

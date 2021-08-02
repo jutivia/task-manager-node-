24367a5c485aea7c89621766695ad0266bb6deb6
jutivia <rukkylaw.tl@gmail.com>
2021-08-02 23:42:16 +0100

    connected front-end to the servers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -33,8 +33,19 @@ const getTask=async (req,res)=>{
     
 }
 
-const updateTask=(req,res)=>{
-    res.send('update task')
+const updateTask= async (req,res)=>{
+    try{
+        const {id:taskId}= req.params
+
+        const task= await Task.findOneAndUpdate({_id:taskId}, req.body, {new:true, runValidators:true})
+ if(!task){
+            return res.status(404).json({msg:`no task with Id ${taskId}`})
+        }
+         res.status(200).json({task})
+        
+    }catch(error){
+         res.status(500).json({msg:error})
+    }
 }
 
 const deleteTask=async (req,res)=>{
